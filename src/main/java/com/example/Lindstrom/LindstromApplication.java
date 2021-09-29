cab63197d142b69145c6b268aa448278fe0329e2
linafre1 <linafredrikssonn@gmail.com>
2021-09-29 14:54:48 +0200

    basics

---
+++ src/main/java/com/example/Lindstrom/LindstromApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lindstrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindstromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindstromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lindstrom/LindstromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lindstrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindstromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
